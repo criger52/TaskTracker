c2321396b437e2fa9c2f1f6203b40158f48d459f
crigerr <vladimirv2312@gmail.com>
2024-11-19 17:38:50 +0300

    Конец практики 19.11.24 Сделано: 1)Реализация поля history_projects(изменено с jsonfield на textfield и при создании проекта добавляется к пользователю)(до конца не прописано например при put запросе, потому что планируется после смены БД, сменить поле на ListField) *Для этого были изменены представление post запроса(project.views) 2)Добавлено приложение task *Прописан сериализатор, модель и добавление в админ панель 3)Добавлено приложение comment(просто приложение, ничего не реализовано) 4)Изменение эндпоинтов для отображения ролей(project.urls)

---
+++ task/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+from rest_framework.fields import URLField
+
+from project.models import Project
+from user.models import DefaultUser
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=16)
+    description = models.TextField()
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)  # поменять на uuid
+    user = models.ForeignKey(DefaultUser, on_delete=models.CASCADE, related_name='usser')  # поменять на uuid
+
+    STATUS_CHOICES = (
+        ('Grooming', 'Grooming'),
+        ('In progress', 'In progress'),
+        ('Dev', 'Dev'),
+        ('Done', 'Done')
+    )
+
+    PRIORITY_CHOICES = (
+        ('Low', 'Низкий'),
+        ('Medium', 'Средний'),
+        ('High', 'Высокий'),
+    )
+
+    status = models.CharField(max_length=16, choices=STATUS_CHOICES)
+    priority = models.CharField(max_length=16, choices=PRIORITY_CHOICES, default='Medium')
+    create_date = models.DateField(auto_now_add=True)
+    last_update_date = models.DateField(auto_now=True)
+    deadline = models.DateField()
+    responsible_fot_test = models.ForeignKey(DefaultUser, on_delete=models.CASCADE)
+
+
+
+    def __str__(self):
+        return self.title
